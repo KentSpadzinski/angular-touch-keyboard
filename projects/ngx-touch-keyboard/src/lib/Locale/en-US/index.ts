38e2f17658aa47e7579e0b37286551ec9fe05a5e
Kent Spadzinski <kent@spadzinski.com>
2024-03-06 07:10:06 -0500

    Update index.ts
    updated standard text / text_shift layout

--- projects/ngx-touch-keyboard/src/lib/Locale/en-US/index.ts
+++ projects/ngx-touch-keyboard/src/lib/Locale/en-US/index.ts
@@ -3,15 +3,17 @@ import { fnDisplay } from '../constants';
 
 const layouts: Layout = {
   text_alphabetic: [
-    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
-    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-    ['{shift}', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '{backspace}'],
+    ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+    ['-', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '{backspace}'],
     ['{numeric}', '{space}', '{done}'],
   ],
   text_shift: [
+    ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
     ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
     ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
-    ['{shift}', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '{backspace}'],
+    ['-', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '{backspace}'],
     ['{numeric}', '{space}', '{done}'],
   ],
   text_numeric: [
